605afebbabe173ab34f296137fd9caa3e1ea2671
josnin <nino.capati@gmail.com>
2024-06-24 21:34:20 +0800

    refactor / optimize / add comments

--- src/counter.ts
+++ src/counter.ts
@@ -2,37 +2,28 @@ import { RedGin, getset, html, watch, event } from 'redgin';
 
 
 export default class Counter2 extends RedGin {
-  
   counter = getset<number>(0);
-  
 
   render() {
     return html`
-    <div>
-    
-    <div class="card">
-    
-      <button
-        ${ event('click', () => this.counter++ ) } 
-        type="button">
-          Counter ${ watch(['counter'], () =>  this.counter  )}
-      </button>
+        <button
+          ${event('click', () => this.counter++)}
+          type="button"
+        >
+          Counter ${watch(['counter'], () => this.counter)}
+        </button>
         
-    
-  </div>
     `;
   }
   
-
-  
-
- 
 }
 
+// Define the custom element 'sample-counter'
 customElements.define('sample-counter', Counter2);
 
+// Declare the custom element globally
 declare global {
-	interface HTMLElementTagNameMap {
-		"sample-counter": Counter2;
-	}
+  interface HTMLElementTagNameMap {
+    "sample-counter": Counter2;
+  }
 }
